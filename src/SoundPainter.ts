01c69015f740c1d03d52716f2466dbaa96190d89
unknown <mbellman@nwe.com>
2021-07-06 00:48:16 -0500

    Bit o' refactoring

--- src/SoundPainter.ts
+++ src/SoundPainter.ts
@@ -5,6 +5,7 @@ import AudioFile from './audio/AudioFile';
 import { clamp } from './utilities';
 
 export default class SoundPainter {
+  private static readonly TOTAL_NOTES: number = 108;
   private audio: AudioFile = null;
   private analyser: Analyser;
   private bufferCanvas: Canvas;
@@ -42,17 +43,53 @@ export default class SoundPainter {
     this.renderInterval = window.setInterval(() => this.render(), 20);
   }
 
-  private getNoteColor(note: number, loudness: number): string {
-    const tone = note / 108;
-    const volume = clamp(loudness / 255, 0.3, 1);
+  /**
+   * Converts analyser frequency data, represented as [0 - 255] decibel
+   * levels at various frequencies, to notes, represented as normalized
+   * [0.0 - 1.0] volumes at various musical keys.
+   */
+  private createNotes(): number[] {
+    this.analyser.refreshData();
+
+    const analyserData = this.analyser.getData();
+    let notes = new Array(SoundPainter.TOTAL_NOTES);
+
+    notes.fill(0);
+
+    // Map frequencies to notes
+    for (let i = 0; i < analyserData.length; i++) {
+      const frequency = i / analyserData.length * AudioCore.SAMPLE_RATE * 0.5;
+      const key = Math.round(12 * Math.log2(frequency / 440)) + 49;
+
+      if (key >= 0 && key < SoundPainter.TOTAL_NOTES) {
+        // Bias higher keys as being equivalently 'louder'
+        // to counteract intrinsically louder lower frequencies
+        const loudnessFactor = clamp(2.0 * key / SoundPainter.TOTAL_NOTES, 0.8, 1.5);
+
+        notes[key] = (analyserData[i] / 255) * loudnessFactor;
+      }
+    }
+
+    const loudestNote = Math.max(...notes);
+
+    // De-emphasize quieter notes to suppress noise artifacts
+    for (let i = 0; i < notes.length; i++) {
+      notes[i] *= Math.pow(notes[i] / loudestNote, 5);
+    }
+
+    return notes;
+  }
+
+  private getKeyColor(key: number, loudness: number): string {
+    const tone = key / SoundPainter.TOTAL_NOTES;
 
     let r = Math.sin(3 * tone * Math.PI);
     let g = Math.sin(3 * tone * Math.PI + 0.5 * Math.PI);
     let b = Math.sin(3 * tone * Math.PI + 1.3 * Math.PI);
 
-    r = Math.round(clamp(r * 255, 0, 255) * volume);
-    g = Math.round(clamp(g * 255, 0, 255) * volume);
-    b = Math.round(clamp(b * 255, 0, 255) * volume);
+    r = Math.round(clamp(r * 255, 0, 255) * loudness);
+    g = Math.round(clamp(g * 255, 0, 255) * loudness);
+    b = Math.round(clamp(b * 255, 0, 255) * loudness);
 
     let rHex = Number.isNaN(r) ? '0' : r.toString(16);
     let gHex = Number.isNaN(g) ? '0' : g.toString(16);
@@ -65,6 +102,9 @@ export default class SoundPainter {
     return `#${rHex}${gHex}${bHex}`;
   }
 
+  /**
+   * @todo move elsewhere
+   */
   private async fileToBlob(file: File): Promise<Blob> {
     return new Promise(resolve => {
       const reader = new FileReader();
@@ -107,48 +147,30 @@ export default class SoundPainter {
   }
 
   private render(): void {
-    this.analyser.refreshData();
+    const notes = this.createNotes();
 
-    const data = this.analyser.getData();
-
-    // this.visibleCanvas.clear();
-
-    let notes = new Array(108);
-
-    notes.fill(0);
-
-    for (let i = 0; i < data.length; i++) {
-      const frequency = i / data.length * AudioCore.SAMPLE_RATE * 0.5;
-      const key = Math.round(12 * Math.log2(frequency / 440)) + 49;
-      const index = clamp(key, 0, 108);
-
-      if (key >= 0 && key <= 108) {
-        const loudnessFactor = clamp(2.0 * index / 108, 0.8, 1.5);
-
-        notes[index] = Math.round(data[i] * loudnessFactor);
-      }
-    }
-
-    const loudestNote = Math.max(...notes);
-
-    for (let i = 0; i < notes.length; i++) {
-      notes[i] = Math.round(notes[i] * Math.pow(notes[i] / loudestNote, 5));
-    }
-
-    for (let i = 0; i < notes.length; i++) {
-      const height = window.innerHeight / notes.length;
-      const y = (notes.length - i - 1) * height;
-      const brightness = Math.round(255 * notes[i] / 255);
-      const color = this.getNoteColor(i, notes[i]);
-
-      this.visibleCanvas.circle(color, this.currentXOffset + Math.random() * 4 - 2, y + Math.random() * 4 - 2, Math.round(brightness / 15));
-    }
+    this.renderNotes(notes);
 
     this.currentXOffset += 10;
 
     if (this.currentXOffset > window.innerWidth) {
       this.currentXOffset = 0;
       this.visibleCanvas.clear();
+    }
+  }
+
+  private renderNotes(notes: number[]): void {
+    const noteHeight = window.innerHeight / notes.length;
+
+    for (let i = 0; i < notes.length; i++) {
+      const y = (notes.length - i - 1) * noteHeight;
+      const brightness = Math.round(255 * notes[i]);
+      const color = this.getKeyColor(i, notes[i]);
+      const xDrift = Math.random() * 4 - 2;
+      const yDrift = Math.random() * 4 - 2;
+      const radius = Math.round(brightness / 15);
+
+      this.visibleCanvas.circle(color, this.currentXOffset + xDrift, y + yDrift, radius);
     }
   }
 
